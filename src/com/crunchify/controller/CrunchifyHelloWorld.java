4407f5f0dee760d2a358b748e657eee914a79fa3
jazyac <jazy_ac@hotmail.com>
2018-02-25 15:22:40 +1300

    making simple api calls to edamam-edamam-nutrition-analysis.p.mashape.com on page load

--- src/com/crunchify/controller/CrunchifyHelloWorld.java
+++ src/com/crunchify/controller/CrunchifyHelloWorld.java
@@ -1,8 +1,15 @@
 package com.crunchify.controller;
  
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.jazyac.healthApp.key.ApiKeyFileReader;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
  
 
  
@@ -15,6 +22,27 @@ public class CrunchifyHelloWorld {
 	@RequestMapping("/ML")
 	public ModelAndView ML() {
  
+		// These code snippets use an open-source library. http://unirest.io/java
+		try {
+
+
+			
+			HttpResponse<String> response = Unirest.get("https://edamam-edamam-nutrition-analysis.p.mashape.com/api/nutrition-data?ingr=1+stick+of+gum")
+					.header("X-Mashape-Key", ApiKeyFileReader.getKey())
+					.header("Accept", "application/json")
+			.asString();
+			
+			
+
+			System.out.println(response.getBody());
+			
+		} catch (UnirestException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+
 
 		return new ModelAndView("ML", "null", null );
 	}
